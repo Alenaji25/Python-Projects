56a6f3beaf9a8df1e3cb18e8da5e9a2a73da2cc2
Alen K Aji <allenaji2512@gmail.com>
2024-08-01 17:49:17 +0530

    Update text_corrector.py

--- text_corrector.py
+++ text_corrector.py
@@ -4,10 +4,10 @@ n=int(input("Enter the no. of words :"))
 for i in range(n):
     x=input("Enter Words :")
     words.append(x)
-corrected_words = []
+correct_words = []
 for i in words:
-    corrected_words.append(TextBlob(i))
+    correct_words.append(TextBlob(i))
 print("Wrong words :", words)
 print("Corrected Words are :")
-for i in corrected_words:
+for i in correct_words:
     print(i.correct(), end=" ")
